422e64efdf2904da12eec7dceace62bc6552eec3
Vitor M Mendonça <monteiromendoncavitor@gmail.com>
2020-12-01 22:27:01 -0300

    Showing examples

---
+++ index.js
@@ -0,0 +1,11 @@
+import Exemple1 from './src/examples/1-example/1-example.js';
+import Exemple2 from './src/examples/2-example/2-example.js';
+import Exemple3 from './src/examples/3-example/3-example.js';
+
+console.log('Example 1: graph image is o src/examples/1-example');
+Exemple1();
+/*console.log('Example 2: graph image is o src/examples/1-example');
+Exemple2();
+console.log('Example 3: graph image is o src/examples/1-example');
+Exemple3();
+*/
